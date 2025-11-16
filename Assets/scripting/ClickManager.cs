3d7f15d171b1a94e526751c5fa6352c39b763772
Hentatiyassine <77630117+Hentatiyassine@users.noreply.github.com>
2025-11-16 20:46:53 +0100

    codepush

---
+++ Assets/scripting/ClickManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public GameObject referenceObject;
+    public Transform myCam;
+
+    Vector3 camPosition;
+    Vector3 cubePosition;
+    Vector3 offset;
+
+    void Start()
+    {
+        cubePosition = referenceObject.transform.position;
+        camPosition = myCam.position;
+        offset = camPosition - cubePosition;
+    }
+
+    void Update()
+    {
+        //myCam.position = getposition();
+        setPosition(myCam);
+    
+        var mr = referenceObject.GetComponent<MeshRenderer>();
+        var mats = mr.materials;
+        mats[0] = referenceObject.GetComponent<CubeScript>().material1;   // drag your material into "secondMaterial" in Inspector
+        mr.materials = mats;
+    }
+
+    public Vector3 getposition()
+    {
+        //
+        return referenceObject.transform.position + offset;
+    }
+
+    public void setPosition(Transform myObjectToSet)
+    {
+        myObjectToSet.position = referenceObject.transform.position + offset;
+    }
+
+    public void cameraLookAtObject()
+    {
+        myCam.LookAt(referenceObject.transform);
+    }
+}
