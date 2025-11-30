1324683dd0b70a46b86c48388bb6223d5fc6e65a
Hentatiyassine <77630117+Hentatiyassine@users.noreply.github.com>
2025-11-30 20:17:18 +0100

    BeforeCode

---
+++ Assets/scripting/Quille.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quille : MonoBehaviour
+{
+    public bool hasFallen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHit()
+    {
+        //
+    }
+}
---
+++ Assets/scripting/ScoreManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int currentScore;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
