8afe3406220c9d5a72ab6c0f667a92fa3cf66f97
slimbenyedder0-cmyk <slim.benyedder0@gmail.com>
2025-11-30 21:17:50 +0100

    Some Scriptin' on quilles

--- Assets/scripting/Quille.cs
+++ Assets/scripting/Quille.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Quille : MonoBehaviour
 {
     public bool hasFallen;
+    public Quaternion cylinderRotation;
+    public Vector3 startRotation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cylinderRotation = transform.rotation;
+        startRotation = new Vector3 (this.transform.rotation.x,this.transform.rotation.y,this.transform.rotation.z);
+        GetHit();
+        StartCoroutine(CheckRotationcoroutine());
     }
 
     // Update is called once per frame
@@ -21,4 +27,24 @@ public class Quille : MonoBehaviour
     {
         //
     }
+
+    public IEnumerator CheckRotationcoroutine()
+    {
+        yield return new WaitForSeconds(0.25f);
+        while(hasFallen==false)
+        {
+            Vector3 actual = new Vector3 (this.transform.rotation.x,this.transform.rotation.y,this.transform.rotation.z);
+             if((actual.x-startRotation.x > 45)||(actual.x-startRotation.x < -45)||(actual.z-startRotation.z < -45)||(actual.z-startRotation.z < -45))
+             {
+                 hasFallen = true;
+             }
+             yield return new WaitForSeconds(0.25f);
+        }
+        
+
+
+    }    
+
+    
 }
+
