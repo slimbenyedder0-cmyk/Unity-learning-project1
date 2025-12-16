bcc5391c0167e130b715ab3eb81efab0e93dd696
slimbenyedder0-cmyk <slim.benyedder0@gmail.com>
2025-12-17 00:39:49 +0100

    Reorganize assets and add catapult prefab/scripts
    Moved animation, prefab, and spritesheet assets into structured subfolders under Assets/Assets. Added new prefabs for 'La Catapult' and related objects, along with their meta files. Introduced new scripts 'Catapult Controller.cs' and 'General Mouse Command.cs' to support catapult functionality.

---
+++ Assets/scripting/Catapult Controller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CatapultController : MonoBehaviour
+{
+    public void ReceiveLookInput (Vector2 lookDelta)
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripting/General Mouse Command.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class General_Mouse_Command : MonoBehaviour
+{
+    public Vector2 mouseDelta;
+    public bool commandSystemEnabler;
+    public InputActionReference lookAction;
+    public void OnEnable ()
+    {
+        lookAction.action.Enable();
+        lookAction.action.performed += OnLook;
+        lookAction.action.canceled += OnLook;
+    }
+
+    private void OnDisable()
+    {
+        lookAction.action.performed -= OnLook;
+        lookAction.action.canceled -= OnLook;
+        lookAction.action.Disable();
+    }
+    private void OnLook(InputAction.CallbackContext ctx)
+    {
+        mouseDelta = ctx.ReadValue<Vector2>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
