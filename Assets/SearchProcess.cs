d339d45847cb0216e42bd9e8ea497124f8c0ff4a
1emeow <lemaotztung@gmail.com>
2025-12-28 18:34:03 +0100

    score prerequisites
    adding a search radius for the cube and a trigger collision for the spirale à points

---
+++ Assets/SearchProcess.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SearchProcess : MonoBehaviour
+{
+    public GameObject Ofinterest;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+   // void OnCollisionEnter(Collision collision)
+    //{
+       // if (Touche == false)
+        //{
+            //if (collision.gameObject == Ofinterest)
+            //{
+                //Debug.Log("aspiration de sprite");
+                //StartCoroutine(Coroutineofcollision());
+                //Touche = true;
+            //}
+        //}
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
